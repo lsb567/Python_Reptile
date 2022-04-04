419e179e810fc341bc91ef93e058ee916da8b1af
lsb567 <1092244357@qq.com>
2022-04-04 17:08:03 +0800

    1

---
+++ day01/02-Request的使用.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from urllib.request import Request
+from random import choice
+
+url = "http://www.baidu.com"
+
+user_agents = ['Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E)',
+               'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.33 Safari/534.3 SE 2.X MetaSr 1.0',
+               'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E; SE 2.X MetaSr 1.0)']
+
+
+headers = {
+    "User-Agent": choice(user_agents)
+}
+
+
+request = Request(url, headers=headers)
+# print(request.get_header('User-agent'))
+response = urlopen(request)
+
+info = response.read()
+
+print(info.decode())
