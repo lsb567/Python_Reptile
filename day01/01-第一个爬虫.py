f30f0c76d424fa5c956be900b2a1f8880ab0a543
lsb567 <1092244357@qq.com>
2022-04-04 16:48:47 +0800

    1

--- day01/01-第一个爬虫.py
+++ day01/01-第一个爬虫.py
@@ -2,8 +2,18 @@ from urllib.request import urlopen
 
 url = "http://www.baidu.com"
 # 发送请求
-respone = urlopen(url)
+response = urlopen(url)
 # 读取内容
-info = respone.read()
+info = response.read()
 # 打印内容
-print(info)
+print(info.decode())
+
+
+# # 打印状态码
+# print(response.getcode())
+# # 打印真实url
+# print('---------------')
+# print(response.geturl())
+# # 打印响应头
+# print('-------------')
+# print(response.info())
