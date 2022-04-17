84d16f9c79b3f324e844ec4ad75a95b95eb41d81
lsb567 <1092244357@qq.com>
2022-04-17 11:57:13 +0800

    1

---
+++ day05/movie2.py
@@ -0,0 +1,73 @@
+import requests
+from fake_useragent import UserAgent
+from lxml import etree
+from random import randint
+from time import sleep
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    headers = {
+        'User-Agent': UserAgent().chrome
+    }
+    # sleep(randint(3, 10))
+    response = requests.get(url, headers=headers)
+    response.encoding = 'utf-8'
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+
+def parse_index(html):
+    soup = BeautifulSoup(html, 'lxml')
+    all_a = soup.select('.channel-detail.movie-item-title > a')
+    all_url = []
+    for a in all_a:
+        all_url.append(a.attrs['href'])
+    # e = etree.HTML(html)
+    # all_url = e.xpath('//div[@class="channel-detail movie-item-title"]/a/@href')
+    # # print(all_url, '1')
+    return ['http://maoyan.com{}'.format(url) for url in all_url]
+
+
+def parse_info(html):
+    soup = BeautifulSoup(html, 'lxml')
+    name = soup.select('h1.name')  # 同样，动态网页获取不到，乌鱼子
+    types = soup.select('li.ellipsis')
+    actors = soup.select('li.celebrity actor > div.info > a')
+
+
+    # e = etree.HTML(html)
+    # name = e.xpath('//h1[@class="name"]/text()')
+    # types = e.xpath('//li[@class="ellipsis"]/a/text()')
+    # actors = e.xpath('//li[@class="celebrity actor"]//div[@class="info"]/a/text()')
+    actors = format_actors(actors)
+    return {
+        'name': name,
+        'types': types,
+        'actors': actors
+    }
+
+
+def format_actors(actors_a):
+    actor_set = set()
+    for a in actors_a:
+        actor_set.add(a.text.strip())
+    return actor_set
+
+
+def main():
+    index_url = 'http://maoyan.com/films'
+    html = get_html(index_url)
+    movie_urls = parse_index(html)
+    print('----')
+    print(movie_urls, 'movie_urls')
+    for url in movie_urls:
+        movie_html = get_html(url)
+        movie = parse_info(movie_html)
+        print(movie, 'movie')  # 网站现在变成动态了，所以获取不到了，乌鱼子
+
+
+if __name__ == '__main__':
+    main()
