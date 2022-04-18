4787c302d32637512618d0362f6bb87b15775342
lsb567 <1092244357@qq.com>
2022-04-18 20:21:43 +0800

    可以使用selenium获取动态页面

--- day06/34-直播练习.py
+++ day06/34-直播练习.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
-
+from time import sleep
 
 driver = webdriver.Chrome()
 url = 'https://www.huya.com/g/lol'
 driver.get(url)
-html = driver.page_source
-names = driver.find_element_by_xpath('//i[@class="nick"]')
-counts = driver.find_element_by_xpath('//i[@class="js-num"]')
-for name, count in zip(names, counts):
-    print(name, ":", count)
-print(html)
+num = 1
+while True:
+    print('第' + str(num) + '页------------------------------------------------')
+    num += 1
+    sleep(2)  # 运行速度过快，可能页面代码还没有加载出来，导致爬取出错
+    html = driver.page_source
+    names = driver.find_elements_by_xpath('//i[@class="nick"]')
+    counts = driver.find_elements_by_xpath('//i[@class="js-num"]')
+    for name, count in zip(names, counts):
+        print(name.text, ":", count.text)
+    if driver.page_source.find('laypage_next') != -1:
+        driver.find_element_by_xpath('//a[@class="laypage_next"]').click()
+    else:
+        break
+
