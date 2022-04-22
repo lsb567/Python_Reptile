f777bce1daf27d76000a831e7167b1eb911cc74e
lsb567 <1092244357@qq.com>
2022-04-22 18:42:09 +0800

    1

---
+++ day06/36-爬虫新写法.py
@@ -0,0 +1,76 @@
+import requests
+from fake_useragent import UserAgent
+# url管理
+class URLManager(object):
+    def __init__(self):
+        self.new_url = []
+        self.old_url = []
+
+    # 获取一个url
+    def get_new_url(self):
+        url = self.new_url.pop()
+        self.old_url.append(url)
+        return url
+
+    # 增加一格url
+    def add_new_url(self, url):
+        if url not in self.new_url and url and url not in self.old_url:
+            self.new_url.append(url)
+
+    # 增加多个url
+    def add_new_urls(self, urls):
+        for url in urls:
+            self.add_new_url(url)
+
+    # 判断是否还有可以爬取的url
+    def has_new_url(self):
+        return self.get_new_url_size() > 0
+
+    # 获取可以爬取的数量
+    def get_new_url_size(self):
+        return len(self.new_url)
+
+    # 获取已经爬取的数量
+    def get_old_url_size(self):
+        return len(self.old_url)
+
+
+# 爬取
+class Downloader:
+    def download(self, url):
+        response = requests.get(self.url, headers={'UserAgen': UserAgent().random})
+        if response.status_code == 200:
+            response.encoding = 'utf-8'
+            return response.text
+        else:
+            return None
+
+
+# 解析
+class Parser:
+    def parse(self, html):
+        pass
+
+
+# 数据处理
+class DataOutput:
+    def save(self, data):
+        pass
+
+
+# 调度
+class DiaoDu:
+    def __init__(self):
+        self.downloader = Downloader()
+        self.url_manager = URLManager()
+        self.parser = Parser()
+        self.data_saver = DataOutput()
+
+    def run(self, url):
+        self.url_manager.add_new_url(url)
+        while self.url_manager.has_new_url():
+            url = self.url_manager.get_new_url()
+            html = self.downloader.download(url)
+            data, urls = self.parser.parse(html)
+            self.data_saver.save(data)
+            self.url_manager.add_new_urls(urls)
