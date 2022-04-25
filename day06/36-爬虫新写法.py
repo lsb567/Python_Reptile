f05e78909f0b010594c9a225331ee892aa125bb9
lsb567 <1092244357@qq.com>
2022-04-25 18:29:23 +0800

    1

--- day06/36-爬虫新写法.py
+++ day06/36-爬虫新写法.py
@@ -1,5 +1,8 @@
 import requests
 from fake_useragent import UserAgent
+from lxml import etree
+
+
 # url管理
 class URLManager(object):
     def __init__(self):
@@ -38,7 +41,7 @@ class URLManager(object):
 # 爬取
 class Downloader:
     def download(self, url):
-        response = requests.get(self.url, headers={'UserAgen': UserAgent().random})
+        response = requests.get(url, headers={'UserAgen': UserAgent().random})
         if response.status_code == 200:
             response.encoding = 'utf-8'
             return response.text
@@ -49,13 +52,25 @@ class Downloader:
 # 解析
 class Parser:
     def parse(self, html):
-        pass
+        e = etree.HTML(html)
+        datas = self.parse_info(e)
+        urls = e.xpath('//div[@class="pagination clearfix"]/a/@href')
+        return datas, urls
+
+    def parse_info(self, e):
+        ps = e.xpath('//div[@class="post-body"]/p')
+        datas = []
+        for p in ps:
+            datas.append(p.xpath('string(.)'))
+        return datas
 
 
 # 数据处理
 class DataOutput:
-    def save(self, data):
-        pass
+    def save(self, datas):
+        with open('duanzi.txt', 'a', encoding='utf-8') as f:
+            for data in datas:
+                f.write(data+'\n')
 
 
 # 调度
@@ -74,3 +89,8 @@ class DiaoDu:
             data, urls = self.parser.parse(html)
             self.data_saver.save(data)
             self.url_manager.add_new_urls(urls)
+
+
+if __name__ == '__main__':
+    diao_du = DiaoDu()
+    diao_du.run('http://www.921131.com/')
