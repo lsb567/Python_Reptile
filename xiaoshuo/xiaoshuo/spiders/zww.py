55be810fc9f30b13ac445c38ae4fe3a57dad467c
lsb567 <1092244357@qq.com>
2022-04-28 17:57:30 +0800

    1

--- xiaoshuo/xiaoshuo/spiders/zww.py
+++ xiaoshuo/xiaoshuo/spiders/zww.py
@@ -4,7 +4,16 @@ import scrapy
 class ZwwSpider(scrapy.Spider):
     name = 'zww'
     allowed_domains = ['86zw.co']
-    start_urls = ['http://86zw.co/']
+    start_urls = ['http://www.86zw.co/xiaoshuo/3334/15353563.html']
 
     def parse(self, response):
-        pass
+        title = response.xpath('//h1/text()').extract_first()
+        content = ''.join(response.xpath('//div[@id="content"]/text()').extract()).replace('    ', '    ')
+
+        yield {
+            'title': title,
+            'content': content
+        }
+        next_url = response.xpath('//div[@class="bottem2"]/a[4]/@href').extract_first()
+        # base_url = 'http://www.86zw.co{}'.format(next_url)
+        yield scrapy.Request(response.urljoin(next_url), callback=self.parse)
